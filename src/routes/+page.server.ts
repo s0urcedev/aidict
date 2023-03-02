f4bfd98d0a750b469f4ee5c0dc4eea3e3af8be65
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-03-02 21:40:53 +0000

    Removed console.log

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,7 +7,6 @@ import type { LayoutServerLoad, LayoutServerLoadEvent } from './$types';
 export const load: LayoutServerLoad = (async ({ locals }: LayoutServerLoadEvent) => {
     try {
         const gotLocals = locals as { cookies: Cookies };
-        console.log(gotLocals);
         if (gotLocals.cookies.get('token') !== undefined && gotLocals.cookies.get('token') !== null && gotLocals.cookies.get('token') !== '') {
             const userToken: UserToken = decodeToken(gotLocals.cookies.get('token') ?? '');
             const user: User | null = await getUser(userToken['email'], userToken['password']);
