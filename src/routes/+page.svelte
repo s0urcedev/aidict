7917dcecb16995931eb91cf823c5959c1000e975
s0urcedev <54136752+s0urcedev@users.noreply.github.com>
2023-03-02 08:07:00 +0000

    1.4.1 Home page update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,54 @@
         if (message !== null) {  
             alert(message);
         }
-        window.location.href = '/my-sets';
     });
 </script>
+
+<style>
+    main {
+        position: relative;
+        display: block;
+        margin: auto;
+        margin-top: 20px;
+        background-color: #FAF5EE;
+        padding: 5px 10px 5px 10px;
+        width: 350px;
+        border-radius: 0.5em;
+        box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+    }
+
+    button {
+        font-family: "e-Ukraine", sans-serif;
+        text-transform: uppercase;
+        text-align: center;
+        display: block;
+        outline: 0;
+        background: #006885;
+        width: 100%;
+        margin: auto;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        border: 0;
+        padding: 10px;
+        color: #FAF5EE;
+        font-weight: 400;
+        font-size: 24px;
+        -webkit-transition: all 0.3 ease;
+        transition: all 0.3 ease;
+        cursor: pointer;
+        border-radius: 0.5em;
+    }
+
+    button:hover {
+        opacity: 0.85;
+    }
+</style>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
+<main>
+    <button><a data-sveltekit-reload href="/my-sets">My sets</a></button>
+    <button><a data-sveltekit-reload href="/login">Authorize</a></button>
+</main>
